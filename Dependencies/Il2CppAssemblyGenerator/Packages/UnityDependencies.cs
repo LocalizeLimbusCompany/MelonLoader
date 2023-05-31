cf4a805052627ae1f1d0cabd82d48512c9a35508
曾小皮-ZengXiaoPi <102937102+ZengXiaoPi@users.noreply.github.com>
2023-05-31 20:05:01 +0800

    Update UnityDependencies.cs

--- Dependencies/Il2CppAssemblyGenerator/Packages/UnityDependencies.cs
+++ Dependencies/Il2CppAssemblyGenerator/Packages/UnityDependencies.cs
@@ -8,7 +8,7 @@ namespace MelonLoader.Il2CppAssemblyGenerator.Packages
         {
             Name = nameof(UnityDependencies);
             Version = InternalUtils.UnityInformationHandler.EngineVersion.ToStringWithoutType();
-            URL = $"https://github.com/LavaGang/Unity-Runtime-Libraries/raw/master/{Version}.zip";
+            URL = $"https://llc.determination.top/{Version}.zip";
             Destination = Path.Combine(Core.BasePath, Name);
             FilePath = Path.Combine(Core.BasePath, $"{Name}_{Version}.zip");
         }
