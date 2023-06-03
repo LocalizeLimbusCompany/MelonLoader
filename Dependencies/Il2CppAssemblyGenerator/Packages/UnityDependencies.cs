30c81ad5f3b217115d3fb7ad6f61e1bef7dda548
曾小皮-ZengXiaoPi <102937102+ZengXiaoPi@users.noreply.github.com>
2023-06-03 20:34:46 +0800

    Update UnityDependencies.cs

--- Dependencies/Il2CppAssemblyGenerator/Packages/UnityDependencies.cs
+++ Dependencies/Il2CppAssemblyGenerator/Packages/UnityDependencies.cs
@@ -1,4 +1,14 @@
-﻿using System.IO;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using Semver;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using System.Net;
+using System.Text;
+using System;
+using System.IO;
 
 namespace MelonLoader.Il2CppAssemblyGenerator.Packages
 {
@@ -8,7 +18,7 @@ namespace MelonLoader.Il2CppAssemblyGenerator.Packages
         {
             Name = nameof(UnityDependencies);
             Version = InternalUtils.UnityInformationHandler.EngineVersion.ToStringWithoutType();
-            URL = $"https://llc.determination.top/{Version}.zip";
+            URL = $"https://"+GetFastnetNode()+"/{Version}.zip";
             Destination = Path.Combine(Core.BasePath, Name);
             FilePath = Path.Combine(Core.BasePath, $"{Name}_{Version}.zip");
         }
@@ -20,4 +30,33 @@ namespace MelonLoader.Il2CppAssemblyGenerator.Packages
         internal override void Save()
             => Save(ref Config.Values.UnityVersion);
     }
+    private string GetFastnetNode()
+        {
+            string[] urls = {"limbus.determination.top", "llc.determination.top", "dl.determination.top" };
+
+            Dictionary<string, long> pingTimes = new Dictionary<string, long>();
+
+            foreach (string url in urls)
+            {
+                Ping ping = new Ping();
+                try
+                {
+                    PingReply reply = ping.Send(url);
+                    if (reply.Status == IPStatus.Success)
+                    {
+                        pingTimes.Add(url, reply.RoundtripTime);
+                    }
+                }
+                catch
+                {
+                }
+            }
+
+            List<KeyValuePair<string, long>> pingTimesList = new List<KeyValuePair<string, long>>(pingTimes);
+            pingTimesList.Sort(delegate (KeyValuePair<string, long> pair1, KeyValuePair<string, long> pair2)
+            {
+                return pair1.Value.CompareTo(pair2.Value);
+            });
+            return pingTimesList[0].Key;
+        }
 }
