a5d736cc883d0ca7ff3aa7848b461c346d1323c9
Bright1192 <2166085463@qq.com>
2023-06-14 01:46:07 +0800

    LLC Logo

---
+++ Dependencies/MelonStartScreen/UI/Themes/UI_Theme_LLC.cs
@@ -0,0 +1,13 @@
+﻿namespace MelonLoader.MelonStartScreen.UI.Themes
+{
+    internal class UI_Theme_LLC : UI_Theme
+    {
+        internal UI_Theme_LLC() => Defaults();
+
+        internal override byte[] GetLoadingImage()
+            => StartScreenResources.LLC_Zero_Logo;
+
+        internal override byte[] GetLogoImage()
+            => StartScreenResources.LLC_Zero_Logo;
+    }
+}
